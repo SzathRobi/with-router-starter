f863c24ad5fc34d0cbbacb2542e990b21547a94c
SzathRobi <szathrobi98@gmail.com>
2022-01-18 11:40:19 +0100

    added react router dom

---
+++ src/routes/Booking.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Booking() {
+  return (
+    <div>
+      <h2>Yeee booking is here</h2>
+      <Link to="/">Go To Home Page</Link>
+    </div>
+  );
+}
+
+export default Booking;
